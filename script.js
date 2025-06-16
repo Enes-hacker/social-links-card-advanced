cb9940f7d7aef1fe6e44e0b75dc435089ca44748
Enes <stu1901261004@uni-plovdiv.bg>
2025-06-16 10:54:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const main = document.getElementById("background");
+    for (let i = 0; i < 200; i++) {
+        const span = document.createElement("span");
+        main.appendChild(span);
+    }
